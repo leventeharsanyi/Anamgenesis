007b1c064353de5d25d7be8a3c32c5840dc36852
harsanyilevente <levente.harsanyi@greehill.com>
2025-12-10 21:40:19 +0100

    Adding a loading from the env file.

--- anamgenesis/app/main.py
+++ anamgenesis/app/main.py
@@ -1,8 +1,15 @@
+import os
 import streamlit as st
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+secret_message = os.getenv("MY_SECRET_MESSAGE", "ERROR!!!!")
 
 st.title("Anamgenesis")
 st.write("You could start the app!")
 btn = st.button("Click me")
 if btn:
+    st.write("Message from .env:")
+    st.code(secret_message)
     st.write(f"Time: {datetime.now()}")
